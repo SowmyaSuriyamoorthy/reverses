385417c1a3623a5c498b7348a621857d4dfe8620
SowmyaSuriyamoorthy <sowmyaselvi22@gmail.com>
2016-07-19 20:58:23 +0530

    Add files via upload

---
+++ Reverses.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Reverses 
+{
+		   public static void main(String[] args)
+		   {
+		      String string="abcdef";
+		      String reverse = new StringBuffer(string).
+		      reverse().toString();
+		      System.out.println("String before reverse:"+string);
+		      System.out.println("String after reverse: "+reverse);
+		   }
+}
